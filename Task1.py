ddb34b1f271839a076934d3446d1658f542c0155
Sneha Singh <sneha.rkt14@gmail.com>
2025-04-12 06:18:31 +0530

    initial commit
    Pyhton code task 1

---
+++ Task1.py
@@ -0,0 +1,16 @@
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+sum= int(a) + int(b)
+print("Addition: ", sum)
+
+sub= int(a) - int(b)
+print("Subtraction:",sub)
+
+
+mul= int(a) * int(b)
+print("Multiplication:",mul)
+
+div=float(a) /float(b)
+print("Division:",div)
